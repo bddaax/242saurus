b660da3dc037d03c50e8450e4ca5bae7a64c263f
bddaax <brenda147052gmail.com>
2024-11-05 13:04:14 +0700

    Deploy website - based on bf32f80a993ac9d8f90756167a693445d270c10d

---
+++ assets/js/f8551aab.d8dc358f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_242_saurus=self.webpackChunk_242_saurus||[]).push([[2224],{1976:(s,e,a)=>{a.r(e),a.d(e,{default:()=>c});a(6540);var u=a(781),r=a(4848);function c(){return(0,r.jsxs)(u.A,{children:[(0,r.jsx)("h1",{children:"My React page"}),(0,r.jsx)("p",{children:"This is a React page"})]})}}}]);
